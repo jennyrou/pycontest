b7f58ee2ea8ba2f82690266e9bc687c1159ad023
Jenny Rouleau <jrouleau@BuffaloComputerGraphics.com>
2019-05-02 12:28:39 -0400

    add travis yml

--- tests/test_hypothesis.py
+++ tests/test_hypothesis.py
@@ -6,9 +6,24 @@ from hypothesis import given, strategies as st
 from pycontest import simulation as sim2d
 from pycontest.utils import momentum, E_kin
 
-@pytest.mark.skip(reason="TODO")                                               
+                                             
 @given(mass1  = st.floats(min_value=.1, max_value=1e3),
        mass2  = st.floats(min_value=.1, max_value=1e3))
 def test_energy_hypothesis(mass1, mass2):
+    
+    domain = ([-2, 12], [0, 3])
+    t_max = 6
+    dt = 0.5
+    loc_0 = np.array([[0, 1.5],[10, 1.5]])
+    vel_0 = np.array([[1, 0], [-1, 0]])
+    radius = 1
+    mass = [mass1, mass2]
 
-    pass
+    loc, vel = sim2d.simulation(t_max, dt, mass, radius, loc_0, vel_0, domain)
+    
+    E_ini = E_kin(vel_0, mass)
+    E_end = E_kin(vel, mass)
+    
+    assert E_ini == E_end
+
+pass
